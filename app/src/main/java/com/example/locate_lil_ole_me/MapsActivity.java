d32bb6472e3f5d59f6854a082a12856db51c0df5
Bryan <bryan.dov.bergman@gmail.com>
2020-11-04 21:02:56 +0200

    added address locating ability

--- app/src/main/java/com/example/locate_lil_ole_me/MapsActivity.java
+++ app/src/main/java/com/example/locate_lil_ole_me/MapsActivity.java
@@ -3,11 +3,10 @@ package com.example.locate_lil_ole_me;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.location.*;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -18,6 +17,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -68,11 +71,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
+
                 Log.i("location123", location.toString());
                 mMap.clear();
                 myLatLng = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(myLatLng).title("You Are Here, Asshole"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(myLatLng));
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+
+                    List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    if(listAddresses != null && listAddresses.size() > 0){
+                        Log.i("place_info: ", listAddresses.get(0).toString());
+                        String address = "";
+
+                        if(listAddresses.get(0).getAdminArea() != null){
+                            address += listAddresses.get(0).getAdminArea() + " ";
+                        }
+
+                        if(listAddresses.get(0).getLocality() != null){
+                            address += listAddresses.get(0).getLocality() + " ";
+                        }
+
+                        if(listAddresses.get(0).getThoroughfare() != null){
+                            address += listAddresses.get(0).getThoroughfare() + " ";
+                        }
+
+                        if(listAddresses.get(0).getPostalCode() != null){
+                            address += listAddresses.get(0).getPostalCode() + " ";
+                        }
+
+                        if(listAddresses.get(0).getAdminArea() != null){
+                            address += listAddresses.get(0).getAdminArea() + " ";
+                        }
+
+                        Toast.makeText(getApplicationContext(), address, Toast.LENGTH_SHORT).show();
+
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         };
 
